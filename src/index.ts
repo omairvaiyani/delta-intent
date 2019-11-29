5c502e1511956ba314fd70ea50cd42c424c73755
Omair Vaiyani <omair.vaiyani@live.co.uk>
2019-11-29 21:18:04 +0000

    add validator to output

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import 'joi-extract-type';
 import { ModelConfiguration } from './api/model-configuration';
-import { getIntentions } from './api/get-intentions';
 import { TypeConfigStore } from './api/type-config-store';
+import { getIntentions } from './api/get-intentions';
 
-export { ModelConfiguration, TypeConfigStore, getIntentions };
+import { FieldConfig as _FieldConfig } from './interfaces/field-config-types';
+import { IntentConfig as _IntentConfig } from './interfaces/intent-config-types';
+import { TypeConfig as _TypeConfig } from './interfaces/custom-types';
+import { Validator as _Validator } from './interfaces/validator-types';
+
+namespace DIInterface {
+  export interface FieldConfig extends _FieldConfig {}
+  export interface IntentConfig extends _IntentConfig {}
+  export interface TypeConfig extends _TypeConfig {}
+
+  export type Validator = _Validator;
+}
+
+export { ModelConfiguration, TypeConfigStore, getIntentions, DIInterface };
